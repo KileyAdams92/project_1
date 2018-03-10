c26104ada338cea9c2ba5719c28382a5685c1ea1
Patrick Kearney <pckearney@yahoo.com>
2018-03-10 14:57:14 -0600

    double AJAX after submit

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -2,10 +2,10 @@
 //Global variables
 //Initial Search variables
   //common for the MusixMatch and YoutTube Data search
-  var artist ="Lady Gaga";
-  var song="Edge of Glory";
+  var artistName ="";
+  var songTitle="";
   //for YouTube Data API
-  var duration = "short"; 
+  var duration = ""; 
 
   //Arrays populated with  YouTube API Data
   //TODO combine into a single array of objects
@@ -13,25 +13,6 @@
   var titleArray = ["Lady Gaga Carpool Karaoke",]
   var videoIdArray = ["X5Cfi7U4eL4"]
   var imageArray = ["https://i.ytimg.com/vi/X5Cfi7U4eL4/hqdefault.jpg",]
-
-  $.ajax({
-    url: "https://www.googleapis.com/youtube/v3/search?key=AIzaSyB213LbFGRmVEeWvM3O3-AVreKCX6uhJXk&q="
-    + artist + "&"+ song + "&part=snippet&type=video&videoDuration=" + duration + "&videoSyndicated=true&maxResults=8",
-    method: "GET"
-    }).then(function(response) {
-    for (i = 0; i < 8; i++){
-        console.log(response.items[i].id.videoId);
-        videoIdArray.push(response.items[i].id.videoId);
-        console.log(response.items[i].snippet.title);
-        titleArray.push(response.items[i].snippset.title);
-        console.log(response.items[i].snippet.thumbnails.high.url);
-        imageArray.push(response.items[i].snippet.thumbnails.high.url);
-        };
-        console.log(titleArray);
-        console.log(videoIdArray);
-        console.log(imageArray);
-        //displayImages();
-    });
 
 $(function() {
   // Wrap every letter in a span
@@ -68,35 +49,57 @@ $(function() {
 
   $("#submit-song").click(function() {
     event.preventDefault();
+    songTitle = $("#song-title").val().trim()
+    artistName = $("#artist").val().trim();
+    duration =$("#inlineFormCustomSelectPref").val();
+    console.log("Song: " + songTitle + ", Artist: " + artistName + ", Duration: " + duration);
+
     addLyrics();
+    ytDataSearch();
+
   });
   //musixmatch API call
   function addLyrics(lyricSpot) {
     $(".lyrics").html("");
-    var songTitle = $("#song-title")
-      .val()
-      .trim();
-    console.log(songTitle);
-    var artistName = $("#artist")
-      .val()
-      .trim();
-    var queryURL =
+    var mmQueryURL =
       "http://api.musixmatch.com/ws/1.1/matcher.lyrics.get?apikey=2ba1698bb3ae560efd18a96c8b13d980&q_track=" +
       songTitle +
       "&q_artist=" +
       artistName;
 
     $.ajax({
-      url: queryURL,
+      url: mmQueryURL,
       method: "GET"
     }).then(function(response) {
       var lyrics = $(this).attr("data-lyrics");
-
       var lyricsBody = JSON.parse(response).message.body.lyrics.lyrics_body;
       console.log(lyricsBody);
-
       $(".lyrics").text(lyricsBody);
     });
   }
+
+    //YouTube Data API call
+  function ytDataSearch(){
+
+    var ytQueryURL =
+    "https://www.googleapis.com/youtube/v3/search?key=AIzaSyB213LbFGRmVEeWvM3O3-AVreKCX6uhJXk&q="+ 
+    artistName + "&"+ songTitle  + "&part=snippet&type=video&videoDuration=" + duration + "&maxResults=8";
+    
+    $.ajax({
+      url: ytQueryURL,
+      method: "GET"
+      }).then(function(response) {
+      for (i = 0; i < 8; i++){
+          videoIdArray.push(response.items[i].id.videoId);
+          titleArray.push(response.items[i].snippet.title);
+          imageArray.push(response.items[i].snippet.thumbnails.high.url);
+          };
+          console.log(titleArray);
+          console.log(videoIdArray);
+          console.log(imageArray);
+          //displayImages();
+    });
+  }
+  
 });
 
