0eed57d0ff5113434a4d4127712a7c628b457a73
Patrick Kearney <pckearney@yahoo.com>
2018-03-10 12:55:48 -0600

    YT AJAX added

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,3 +1,36 @@
+//Global variables
+//Initial Search variables
+  //common for the MusixMatch and YoutTube Data search
+  var artist ="Lady Gaga";
+  var song="Edge of Glory";
+  //for YouTube Data API
+  var duration = "short"; 
+
+  //Arrays populated with  YouTube API Data
+  //TODO combine into a single array of objects
+  //TODO remove the inital values once rendering works. 
+  var titleArray = ["Lady Gaga Carpool Karaoke",]
+  var videoIdArray = ["X5Cfi7U4eL4"]
+  var imageArray = ["https://i.ytimg.com/vi/X5Cfi7U4eL4/hqdefault.jpg",]
+
+  $.ajax({
+    url: "https://www.googleapis.com/youtube/v3/search?key=AIzaSyB213LbFGRmVEeWvM3O3-AVreKCX6uhJXk&q="
+    + artist + "&"+ song + "&part=snippet&type=video&videoDuration=" + duration + "&videoSyndicated=true&maxResults=8",
+    method: "GET"
+    }).then(function(response) {
+    for (i = 0; i < 8; i++){
+        console.log(response.items[i].id.videoId);
+        videoIdArray.push(response.items[i].id.videoId);
+        console.log(response.items[i].snippet.title);
+        titleArray.push(response.items[i].snippset.title);
+        console.log(response.items[i].snippet.thumbnails.high.url);
+        imageArray.push(response.items[i].snippet.thumbnails.high.url);
+        };
+        console.log(titleArray);
+        console.log(videoIdArray);
+        console.log(imageArray);
+        //displayImages();
+    });
 // Wrap every letter in a span
 $(".ml7 .letters").each(function() {
   $(this).html(
@@ -42,5 +75,6 @@ anime
 //       method: "GET"
 //     }).then(function(response) {
 //       var lyrics = $(this).attr("data-lyrics");
-//       var result = response.data;
-//       console.log(result);
+//       var lyricsResult = response.data;
+//       console.log(lyricsResult);
+
