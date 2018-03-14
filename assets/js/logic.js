650a3b4ff200c798c5f6ed6b36b3536ea70b1d4d
Kiley Adams <Kileyadams92@gmail.com>
2018-03-13 20:30:51 -0500

    modified sizing of dynamic cards and containers

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -22,9 +22,9 @@ var duration = "";
 var ytSearchArray = []; //Array of objects populated with YouTube API Data
 var ytArrayNum; // the index number of the selected video in the array.
 
-$(function () {
+$(function() {
   // Wrap every letter in a span
-  $(".ml7 .letters").each(function () {
+  $(".ml7 .letters").each(function() {
     $(this).html(
       $(this)
         .text()
@@ -42,7 +42,7 @@ $(function () {
       rotateZ: [180, 0],
       duration: 750,
       easing: "easeOutExpo",
-      delay: function (el, i) {
+      delay: function(el, i) {
         return 50 * i;
       }
     })
@@ -55,16 +55,26 @@ $(function () {
     });
 
   //Triggers both musicmatch and YouTube-Data APIs
-  $("#submit-song").click(function () {
+  $("#submit-song").click(function() {
     event.preventDefault();
-    songTitle = $("#song-title").val().trim()
-    artistName = $("#artist").val().trim();
+    songTitle = $("#song-title")
+      .val()
+      .trim();
+    artistName = $("#artist")
+      .val()
+      .trim();
     duration = $("#inlineFormCustomSelectPref").val();
-    console.log("Song: " + songTitle + ", Artist: " + artistName + ", Duration: " + duration);
+    console.log(
+      "Song: " +
+        songTitle +
+        ", Artist: " +
+        artistName +
+        ", Duration: " +
+        duration
+    );
 
     addLyrics();
     ytDataSearch();
-
   });
   //musixmatch API call
   function addLyrics(lyricSpot) {
@@ -78,7 +88,7 @@ $(function () {
     $.ajax({
       url: mmQueryURL,
       method: "GET"
-    }).then(function (response) {
+    }).then(function(response) {
       var lyrics = $(this).attr("data-lyrics");
       var lyricsBody = JSON.parse(response).message.body.lyrics.lyrics_body;
       console.log(lyricsBody);
@@ -88,16 +98,21 @@ $(function () {
 
   //YouTube Data API call
   function ytDataSearch() {
-
     ytSearchArray = [];
 
-    var ytQueryURL = "https://www.googleapis.com/youtube/v3/search?key=AIzaSyB213LbFGRmVEeWvM3O3-AVreKCX6uhJXk&q=" +
-      artistName + "&" + songTitle + "&part=snippet&type=video&videoDuration=" + duration + "&maxResults=4"
+    var ytQueryURL =
+      "https://www.googleapis.com/youtube/v3/search?key=AIzaSyB213LbFGRmVEeWvM3O3-AVreKCX6uhJXk&q=" +
+      artistName +
+      "&" +
+      songTitle +
+      "&part=snippet&type=video&videoDuration=" +
+      duration +
+      "&maxResults=4";
 
     $.ajax({
       url: ytQueryURL,
       method: "GET"
-    }).then(function (response) {
+    }).then(function(response) {
       for (i = 0; i < 4; i++) {
         var ytInfo = response.items[i];
         var objTitle = ytInfo.snippet.title;
@@ -106,14 +121,14 @@ $(function () {
         var objSmImage = ytInfo.snippet.thumbnails.default.url; //small image for final playlist
         var objMdImage = ytInfo.snippet.thumbnails.medium.url; //medium image for selected video
 
-        ytSearchArray.push({ 
-          ytTitle: objTitle, 
-          ytVideo: objVideo, 
-          ytImage: objImage, 
+        ytSearchArray.push({
+          ytTitle: objTitle,
+          ytVideo: objVideo,
+          ytImage: objImage,
           ytSmImage: objSmImage,
           ytMdImage: objMdImage
         });
-      };
+      }
       console.log(ytSearchArray);
       console.log(ytSearchArray.length);
       console.log('"' + ytSearchArray.length + '"');
@@ -128,21 +143,27 @@ $(function () {
     //repopulates the video-selection div with the current search results;
     for (i = 0; i < ytSearchArray.length; i++) {
       //Creates a Bootstrap Card for each video
-      var videoCard = $("<div>").addClass("card").attr("style", "width: 18rem;");
+      var videoCard = $("<div>")
+        .addClass("card")
+        .attr("style", "width: 17rem;");
 
       //Creates the Card image
-      var cardImage = $("<img>").addClass("card-img-top")
+      var cardImage = $("<img>").addClass("card-img-top");
       cardImage.attr("src", ytSearchArray[i].ytImage);
       cardImage.attr("alt", "Card image cap");
 
       //Creates the Card Body
       var cardBody = $("<div>").addClass("card-body");
-      var cardHeader = $("<h5>").addClass("card-title").text("Video Title:");
+      var cardHeader = $("<h5>")
+        .addClass("card-title")
+        .text("Video Title:");
 
       var cardText = $("<p>").addClass("card-text");
       cardText.text(ytSearchArray[i].ytTitle);
 
-      var cardButton = $("<button>").addClass("btn btn-primary video-selected").text("Select");
+      var cardButton = $("<button>")
+        .addClass("btn btn-primary video-selected")
+        .text("Select");
       //sets a value for each button equql to position in the ytSearchArray
       cardButton.val([i]);
       console.log(cardButton.val());
@@ -150,13 +171,13 @@ $(function () {
       //Assembles the Card and displays to page
       cardBody.append(cardHeader, cardText, cardButton);
       videoCard.append(cardImage, cardBody);
-      $(".video-selection").append(videoCard)
+      $(".video-selection").append(videoCard);
       $(".video-box").text("Select a video to Preview!");
     }
   }
 
-  //listens for a video selection to be made. 
-  $(document).on("click", ".video-selected", function (event) {
+  //listens for a video selection to be made.
+  $(document).on("click", ".video-selected", function(event) {
     ytArrayNum = parseInt($(this).val());
     $(".video-box").text("Review your video below!");
     $(".video-selection").empty();
@@ -173,20 +194,24 @@ $(function () {
     var selectedImage = $("<img>").attr("id", "selected-image");
     selectedImage.attr("src", ytSearchArray[ytArrayNum].ytImage);
 
-    var selectedTitle = $("<div>").attr("id", "selected-title")
+    var selectedTitle = $("<div>").attr("id", "selected-title");
     selectedTitle.text(ytSearchArray[ytArrayNum].ytTitle);
-    
+
     var selectedForm = $("<form>").addClass("form");
 
     var selectedDiv1 = $("<div>").addClass("form-group");
-    var selectedLabel1 = $("<label>").attr("for", "user-name").text("Added By: (your name)");
-    var selectedInput1 = $("<input>").addClass("form-control")
+    var selectedLabel1 = $("<label>")
+      .attr("for", "user-name")
+      .text("Added By: (your name)");
+    var selectedInput1 = $("<input>").addClass("form-control");
     selectedInput1.attr("id", "user-name").attr("type", "text");
     selectedDiv1.append(selectedLabel1, selectedInput1);
 
     var selectedDiv2 = $("<div>").addClass("form-group");
-    var selectedLabel2 = $("<label>").attr("for", "user-comment").text("Comment:");
-    var selectedInput2 = $("<input>").addClass("form-control")
+    var selectedLabel2 = $("<label>")
+      .attr("for", "user-comment")
+      .text("Comment:");
+    var selectedInput2 = $("<input>").addClass("form-control");
     selectedInput2.attr("id", "user-comment").attr("type", "text");
     selectedDiv2.append(selectedLabel2, selectedInput2);
 
@@ -194,39 +219,50 @@ $(function () {
 
     //Button that will add the video to playlist in FireBase
 
-    var selectedButtons = $("<div>").addClass("selected-buttons")
+    var selectedButtons = $("<div>").addClass("selected-buttons");
 
-    var addVideo = $("<button>").addClass("btn btn-success").text("Upload");
+    var addVideo = $("<button>")
+      .addClass("btn btn-success")
+      .text("Upload");
     addVideo.attr("type", "submit");
     addVideo.attr("id", "upload-video");
 
-    var selectAgain = $("<button>").addClass("btn btn-danger").text("Reselect");
+    var selectAgain = $("<button>")
+      .addClass("btn btn-danger")
+      .text("Reselect");
     selectAgain.attr("type", "submit");
     selectAgain.attr("id", "reselect-video");
 
     selectedButtons.append(addVideo, selectAgain);
 
-    $(".video-info-display").append(selectedImage, selectedTitle, selectedForm, selectedButtons);
+    $(".video-info-display").append(
+      selectedImage,
+      selectedTitle,
+      selectedForm,
+      selectedButtons
+    );
   }
-  $(document).on("click", "#upload-video", function (event) {
+  $(document).on("click", "#upload-video", function(event) {
+    var addedBy = $("#user-name")
+      .val()
+      .trim();
+    var userComment = $("#user-comment")
+      .val()
+      .trim();
 
-    var addedBy = $("#user-name").val().trim();
-    var userComment = $("#user-comment").val().trim();
+    pushToFirebase(addedBy, userComment);
 
-    pushToFirebase(addedBy,userComment);
- 
     $(".video-box").text("Search again or play the awesome playlist!");
     $(".video-info-display").empty();
-    $(".video-playback").empty(); //nothing in this yet but there will be. 
-  });  
+    $(".video-playback").empty(); //nothing in this yet but there will be.
+  });
 
   //Clears the selected video and redisplays the original search list.
-  $(document).on("click", "#reselect-video", function (event) {
+  $(document).on("click", "#reselect-video", function(event) {
     videoSearchResults();
   });
 
   function pushToFirebase(varName, varComment) {
-   
     database.ref("/music").push({
       videoId: ytSearchArray[ytArrayNum].ytVideo,
       videoTitle: ytSearchArray[ytArrayNum].ytTitle,
@@ -238,7 +274,6 @@ $(function () {
       searchedArtistName: artistName,
       searchedSongTitle: songTitle,
       dateAdded: firebase.database.ServerValue.TIMESTAMP
-      });
-    }
-  
+    });
+  }
 });
